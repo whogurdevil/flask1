0751b121a0663db6c95e88fa96145e6436066783
Gurdev13 <106698199+Gurdev13@users.noreply.github.com>
2022-07-16 23:28:56 +0530

    I am beginner in this
    flask

---
+++ app.py
@@ -0,0 +1,31 @@
+from cProfile import run
+from email.policy import default
+import imp
+from itertools import product
+from pickle import TRUE
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import false, true
+from datetime import datetime   
+app=Flask(__name__)
+app.config['SQlAlCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+    #return 'Hello WORLD'
+
+@app.route('/products')
+def products():
+    return 'this is a product page'
