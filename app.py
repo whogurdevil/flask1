d3f004cca364900a82a8f83b88de9ba586d31317
Gurdev13 <106698199+Gurdev13@users.noreply.github.com>
2022-07-25 02:07:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-from cProfile import run
-from email.policy import default
-import imp
-from itertools import product
-from pickle import TRUE
-from turtle import title
-from flask import Flask, redirect, render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc, false, true
-from datetime import datetime   
-app=Flask(__name__)
+from datetime import datetime
+
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -25,34 +19,33 @@ class Todo(db.Model):
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method=='POST':
-        title=(request.form['title'])
-        desc=(request.form['desc'])
-        
+        title = request.form['title']
+        desc = request.form['desc']
         todo = Todo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
-    allTodo = Todo.query.all()
+        
+    allTodo = Todo.query.all() 
     return render_template('index.html', allTodo=allTodo)
-    
 
 @app.route('/show')
 def products():
     allTodo = Todo.query.all()
     print(allTodo)
-    return 'this is a product page'
+    return 'this is products page'
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
     if request.method=='POST':
-        title=(request.form['title'])
-        desc=(request.form['desc'])
+        title = request.form['title']
+        desc = request.form['desc']
         todo = Todo.query.filter_by(sno=sno).first()
         todo.title = title
         todo.desc = desc
         db.session.add(todo)
         db.session.commit()
         return redirect("/")
-
+        
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
@@ -63,4 +56,5 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
-
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
